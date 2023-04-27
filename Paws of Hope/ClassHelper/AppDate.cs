10d2d254e368bffbc0bf70eaa9e149753a8007f2
Антон Федяев <fearthe54@gmail.com>
2023-04-27 17:24:30 +0300

    Добавьте файлы проекта.

---
+++ Paws of Hope/ClassHelper/AppDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paws_of_Hope.Class
+{
+    class AppDate
+    {
+        public static EF.AnimalShelterEntities Context { get; } = new EF.AnimalShelterEntities();
+    }
+}
---
+++ Paws of Hope/ClassHelper/HardTextSup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paws_of_Hope.EF
+{
+    public partial class Tutor
+    {
+        public string GetTutor { get => $"Наставник: {TutorFIO.LastName} {TutorFIO.FirstName} {TutorFIO.Patronymic}"; }
+
+    }
+}
